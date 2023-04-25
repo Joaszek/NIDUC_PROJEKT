fad3a7d82a2aaf458cdaab4417f10e5fb003f2e0
Damian <dj.damianjablonski@gmail.com>
2023-04-25 03:03:20 +0200

    CRC changes

--- randGen.py
+++ randGen.py
@@ -8,13 +8,17 @@ class RandGen:
     def __init__(self, seed):
         self.seed = seed
         self.a = 15.34
-        self.c = 1013904223
+        self.c = 1013904223.07
         self.m = 2 ** 32
 
     # method for generating numbers
     def random(self):
         self.seed = (self.a * self.seed + self.c) % self.m
-        return self.seed
+        return int(self.seed % 2)
+
+    def random_zero_to_one(self):
+        self.seed = (self.a * self.seed + self.c) % self.m
+        return self.seed / 2 ** 32
 
 
 generator = RandGen(time.time())
